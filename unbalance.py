b09956cc828ba575d97e812ff0e7d2d32d130fd5
Joakim Pettersson <joakim.pettersson@join.se>
2023-07-17 00:00:56 +0200

    Fixup! Focus training on frequent but difficult-to-detect objects
    Max 88 characters per line.
    Use venv.

--- unbalance.py
+++ unbalance.py
@@ -1,12 +1,12 @@
 """Symlink only non-augmented files
 
-This will make training faster, more focussed on what is normal, therefore more relevant for what we
-have problems with now: bottle-top.
+This will make training faster, more focussed on what is normal, and therefore more
+relevant for a smallest and most frequent object (here: bottle-top).
 
 The symlinks are output into a subdirectory normalized/.
 
-Note that not even the original is kept when there are augmentations of it. This is because it seems most of the
-augmentations are of weird originals, and that the original is not immediately identifiable.
+Note that not even the original is kept when there are augmentations of it. This is
+because the augmentations were created from far too infrequent originals to be relevant.
 
 Dependencies:
 numpy
@@ -21,15 +21,16 @@ import numpy as np
 if __name__ == '__main__':
     import argparse
 
-    argparser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                        description=__doc__,
-                                        epilog="""Examples:
+    argparser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=__doc__,
+        epilog="""Examples:
 $ rm -rf normalized/ other_dir/ keep_aug_* dummy_aug_*
 $ touch keep_aug_0.jpg dummy_aug_0.jpg dummy_aug_1.jpg
-$ python3 unbalance.py *_aug_?.jpg
+$ venv/bin/python unbalance.py *_aug_?.jpg
 normalized/keep_aug_0.jpg -> ../keep_aug_0.jpg
 
-$ python3 unbalance.py normalized/*_aug_?.jpg -o other_dir
+$ venv/bin/python unbalance.py normalized/*_aug_?.jpg -o other_dir
 other_dir/keep_aug_0.jpg -> ../normalized/keep_aug_0.jpg
 """)
     makemake.add_arguments(argparser)
@@ -40,17 +41,18 @@ other_dir/keep_aug_0.jpg -> ../normalized/keep_aug_0.jpg
                            help="Output directory for unbalanced images (normalized)")
     args = argparser.parse_args()
 
-    files, exts = np.array(tuple(zip(* map(os.path.splitext, args.aug_file))), dtype=str)
-    filebases, augs = np.array([file.split('_aug_')[-2:] for file in files], dtype=str).T
+    files, exts = np.array(tuple(zip(*map(os.path.splitext, args.aug_file))), dtype=str)
+    bases, augs = np.array([file.split('_aug_')[-2:] for file in files], dtype=str).T
 
-    for filebase, aug, ext, aug_file in zip(filebases, augs, exts, args.aug_file):
-        if aug == "0" and not ((filebases == filebase) & (augs == "1") & (exts == ext)).any():
+    for base, aug, ext, aug_file in zip(bases, augs, exts, args.aug_file):
+        if aug == "0" and not ((bases == base) & (augs == "1") & (exts == ext)).any():
             if not os.path.isdir(args.output_dir):
                 os.makedirs(args.output_dir)
 
             aug_dir, aug_filename = os.path.split(aug_file)
             path = os.path.join(args.output_dir, aug_filename)
-            reverse_path = os.path.relpath(os.path.abspath(aug_file), os.path.abspath(args.output_dir or "."))
+            reverse_path = os.path.relpath(os.path.abspath(aug_file),
+                                           os.path.abspath(args.output_dir or "."))
             if os.path.exists(path):
                 os.remove(path)
 
