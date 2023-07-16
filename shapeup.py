c91bf1ce99049e0d4a5a8eb522c37507b96a6ff1
Joakim Pettersson <joakim.pettersson@join.se>
2023-07-17 00:03:23 +0200

    Fixup! Include the natural surrounding
    Max 88 characters per line.
    Use venv.

--- shapeup.py
+++ shapeup.py
@@ -3,7 +3,8 @@
 Defaults to shaping up the bottle-top bounding boxes that include only the capsules
 to use a larger and more bottle-including size.
 
-This will make it less similar to can-top (missed detections) or bottle-top glare (double detections).
+This will make it less similar to can-top (missed detections) or bottle-top glare
+(double detections).
 
 The normalized file is output into a subdirectory normalized/ next to the file.
 
@@ -44,12 +45,14 @@ class Norm:
     'bottle-top_has_bottle_and_also_its_box_slot.xml'
     >>> norm(norm.template, copy=True).template
     'normalized/bottle-top_has_bottle_and_also_its_box_slot.xml'
-    """
+    """  # noqa: E501
 
     def __repr__(self):
-        return self.__class__.__name__ + repr((self.template, self.objects, self.normal, self.small))
+        return self.__class__.__name__ + repr(
+            (self.template, self.objects, self.normal, self.small))
 
-    def __init__(self, template='bottle-top_has_bottle_and_also_its_box_slot.xml', objects=['can-top', 'bottle-top'],
+    def __init__(self, template='bottle-top_has_bottle_and_also_its_box_slot.xml',
+                 objects=['can-top', 'bottle-top'],
                  normal=None, small=None):
         """Normal and small bndbox size for objects in template matching these names"""
         self.template = template
@@ -58,19 +61,23 @@ class Norm:
             self.normal, self.small = normal, small
             return
 
-        matches = (o for o in ET.parse(template).getroot().iter('object') if o.findtext('name') in objects)
+        matches = (o for o in ET.parse(template).getroot().iter('object')
+                   if o.findtext('name') in objects)
         bndboxes = [o.find('bndbox') for o in matches]
         if not bndboxes:
             self.normal = self.small = None
             return
 
-        dX = [int(bndbox.findtext('xmax')) - int(bndbox.findtext('xmin')) for bndbox in bndboxes]
-        dY = [int(bndbox.findtext('ymax')) - int(bndbox.findtext('ymin')) for bndbox in bndboxes]
+        dX = [int(bndbox.findtext('xmax')) - int(bndbox.findtext('xmin'))
+              for bndbox in bndboxes]
+        dY = [int(bndbox.findtext('ymax')) - int(bndbox.findtext('ymin'))
+              for bndbox in bndboxes]
 
         self.normal = (median(dX), median(dY))
         self.small = (min(dX), min(dY))
 
-    def __call__(self, sloppy, names=['bottle-top'], difficult=True, copy=False, normalized_dir='normalized'):
+    def __call__(self, sloppy, names=['bottle-top'], difficult=True, copy=False,
+                 normalized_dir='normalized'):
         """Shapeup the bndbox of objects matching these names"""
         voc = ET.parse(sloppy)
         root = voc.getroot()
@@ -119,39 +126,53 @@ class Norm:
 if __name__ == '__main__':
     import argparse
 
-    argparser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                        description=__doc__,
-                                        epilog="""Examples:
+    argparser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=__doc__,
+        epilog="""Examples:
 $ rm -rf normalized
-$ python3 shapeup.py *bottle*.xml  # Use together with the default template
+$ venv/bin/python shapeup.py *bottle*.xml
 bottle-top_has_bottle_and_also_its_box_slot.xml
 single_bottle-top_capsule_in_single_box.xml
 Shapeup bottle-top size (21, 33) to (37.0, 35.5)
 
-$ ls normalized/  # If files were up-shaped, they will appear in this subfolder next to the original.
+$ ls normalized/
 single_bottle-top_capsule_in_single_box.xml
 
-$ python3 shapeup.py -c *bottle*.xml > normalized/log.txt # Copy/modify all files, documenting what was done to them.
+$ venv/bin/python shapeup.py -c *bottle*.xml > normalized/log.txt
 $ ls normalized/
 bottle-top_has_bottle_and_also_its_box_slot.xml
 log.txt
 single_bottle-top_capsule_in_single_box.xml
 """)
     makemake.add_arguments(argparser)
-    argparser.add_argument('-t', '--template', action='store',
-                           default=os.path.join(makemake.prog_dir, 'bottle-top_has_bottle_and_also_its_box_slot.xml'),
-                           help="Template pascal voc xml file for defining normal and smallest bndbox sizes (bottle-top_has_bottle_and_also_its_box_slot.xml)")
-    argparser.add_argument('-to', '--template_object', action='store', default=['can-top', 'bottle-top'],
-                           nargs='+', help="Template object(s) for defining normal and smallest bndbox sizes (can-top bottle-top)")
-    argparser.add_argument('-o', '--object', action='store', default=['bottle-top'],
-                           nargs='+', help="Object(s) to shape-up (bottle-top)")
-    argparser.add_argument('-d', '--difficult', action='store', default=1,
-                           type=int, help="Mark shaped-up objects difficult (1)")
-    argparser.add_argument('-c', '--copy', action='store_true', help="Create a copy even if not modified")
-    argparser.add_argument('-n', '--normalized_dir', action='store', default='normalized',
-                           help="Target directory for normalized files")
-    argparser.add_argument('voc_file', action='store',
-                           nargs='+', help="Pascal voc xml file to shape-up (*.xml)")
+    argparser.add_argument(
+        '-t', '--template', action='store',
+        default=os.path.join(
+            makemake.module_dir, 'bottle-top_has_bottle_and_also_its_box_slot.xml'),
+        help=(
+            "Template pascal voc xml file for defining normal and smallest bndbox sizes"
+            " (bottle-top_has_bottle_and_also_its_box_slot.xml)"))
+    argparser.add_argument(
+        '-to', '--template_object', action='store',
+        default=['can-top', 'bottle-top'], nargs='+', help=(
+            "Template object(s) for defining normal and smallest bndbox sizes"
+            " (can-top bottle-top)"))
+    argparser.add_argument(
+        '-o', '--object', action='store',
+        default=['bottle-top'], nargs='+', help="Object(s) to shape-up (bottle-top)")
+    argparser.add_argument(
+        '-d', '--difficult', action='store', default=1,
+        type=int, help="Mark shaped-up objects difficult (1)")
+    argparser.add_argument(
+        '-c', '--copy', action='store_true',
+        help="Create a copy even if not modified")
+    argparser.add_argument(
+        '-n', '--normalized_dir', action='store',
+        default='normalized', help="Target directory for normalized files")
+    argparser.add_argument(
+        'voc_file', action='store',
+        nargs='+', help="Pascal voc xml file to shape-up (*.xml)")
     args = argparser.parse_args()
 
     voc_files = args.voc_file
